1232d648855d3a2025690f47353b2b1b6f565c76
SashaBohdanov <oleksandr.bohdanov.ik.2023@lpnu.ua>
2023-10-05 10:08:25 +0300

    first branch_02 commit

---
+++ Pr3.3/Pr3.3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double x; // вхідний аргумент
+    double pi = 3.14159265358979323846; // число пі
+
+    double R; // вхідний параметр
+    double y; // результат обчислення виразу
+    cout << "R = "; cin >> R;
+    cout << "x = "; cin >> x;
+
+    // розгалуження в повній формі
+    if (x <= -7 * (-R))
+        y = R;
+    else if (-7 - R < x && x <= -7 + R)
+        y = -sqrt(R) - (x + 7) * (x + 7) + R;
+    else if (-7 + R < x && x <= -4)
+        y = -R;
+    else if (-4 < x && x <= 0)
+        y = -(x - R) / 4;
+    else if (0 < x && x <= pi)
+        y = sin(x);
+    else
+        y = tan(45 * pi / 180) * x - pi;
+
+    cout << endl;
+    cout << "y = " << y << endl;
+    cin.get();
+    return 0;
+}
